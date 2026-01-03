22bd1eb6dbb3083c1a1c43f7c630d5ddc857ef6a
Lai-YT <381xvmvbib@gmail.com>
2026-01-03 19:58:45 +0800

    Complete training

--- project/run_fast_tensor.py
+++ project/run_fast_tensor.py
@@ -29,11 +29,9 @@ class Network(minitorch.Module):
         self.layer3 = Linear(hidden, 1, backend)
 
     def forward(self, x):
-        # ASSIGN3.5
-        h = self.layer1.forward(x).relu()
-        h = self.layer2.forward(h).relu()
-        return self.layer3.forward(h).sigmoid()
-        # END ASSIGN3.5
+        middle = self.layer1(x).relu()
+        middle = self.layer2(middle).relu()
+        return self.layer3(middle).sigmoid()
 
 
 class Linear(minitorch.Module):
@@ -46,10 +44,7 @@ class Linear(minitorch.Module):
         self.out_size = out_size
 
     def forward(self, x):
-        # ASSIGN3.5
-        batch, in_size = x.shape
-        return x.view(batch, in_size) @ self.weights.value + self.bias.value
-        # END ASSIGN3.5
+        return x @ self.weights.value + self.bias.value
 
 
 class FastTrain:
@@ -65,7 +60,6 @@ class FastTrain:
         return self.model.forward(minitorch.tensor(X, backend=self.backend))
 
     def train(self, data, learning_rate, max_epochs=500, log_fn=default_log_fn):
-
         self.model = Network(self.hidden_layers, self.backend)
         optim = minitorch.SGD(self.model.parameters(), learning_rate)
         BATCH = 10
@@ -122,7 +116,7 @@ if __name__ == "__main__":
     if args.DATASET == "xor":
         data = minitorch.datasets["Xor"](PTS)
     elif args.DATASET == "simple":
-        data = minitorch.datasets["Simple"].simple(PTS)
+        data = minitorch.datasets["Simple"](PTS)
     elif args.DATASET == "split":
         data = minitorch.datasets["Split"](PTS)
 
